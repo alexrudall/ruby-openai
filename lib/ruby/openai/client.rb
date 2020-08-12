16001cfbc272a53c202e5fdb3c5db04e6790e1e7
Alex <alexrudall@users.noreply.github.com>
2020-08-12 11:46:20 +0100

    Add method to call API

--- lib/ruby/openai/client.rb
+++ lib/ruby/openai/client.rb
@@ -3,5 +3,7 @@ module OpenAI
     def initialize(access_token: nil)
       @access_token = access_token || ENV["OPENAI_ACCESS_TOKEN"]
     end
+
+    def call(prompt:); end
   end
 end
--- spec/ruby/openai/client_spec.rb
+++ spec/ruby/openai/client_spec.rb
@@ -2,4 +2,9 @@ RSpec.describe OpenAI::Client do
   it "can be initialized" do
     expect { OpenAI::Client.new }.not_to raise_error
   end
+
+  it "can make a request to the OpenAI API" do
+    client = OpenAI::Client.new
+    expect { client.call(prompt: "Once upon a time") }.not_to raise_error
+  end
 end
