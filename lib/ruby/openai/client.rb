8f8647321f8c7acb088563f7eabcd0f4e1fc59b0
Alex <alexrudall@users.noreply.github.com>
2020-08-13 11:58:55 +0100

    Connect to the API!

--- lib/ruby/openai/client.rb
+++ lib/ruby/openai/client.rb
@@ -1,9 +1,24 @@
 module OpenAI
   class Client
+    include HTTParty
+    base_uri "https://api.openai.com/v1/engines"
+
     def initialize(access_token: nil)
       @access_token = access_token || ENV["OPENAI_ACCESS_TOKEN"]
     end
 
-    def call(prompt:); end
+    def call(engine:, prompt:, max_tokens:)
+      self.class.post(
+        "/#{engine}/completions",
+        headers: {
+          "Content-Type" => "application/json",
+          "Authorization" => "Bearer #{@access_token}"
+        },
+        body: {
+          prompt: prompt,
+          max_tokens: max_tokens
+        }.to_json
+      )
+    end
   end
 end
--- spec/ruby/openai/client_spec.rb
+++ spec/ruby/openai/client_spec.rb
@@ -3,8 +3,16 @@ RSpec.describe OpenAI::Client do
     expect { OpenAI::Client.new }.not_to raise_error
   end
 
-  it "can make a request to the OpenAI API" do
-    client = OpenAI::Client.new
-    expect { client.call(prompt: "Once upon a time") }.not_to raise_error
+  context "with an engine, prompt and max_tokens" do
+    let(:engine) { "davinci" }
+    let(:prompt) { "Once upon a time" }
+    let(:max_tokens) { 5 }
+
+    it "can make a request to the OpenAI API" do
+      client = OpenAI::Client.new
+      response = client.call(engine: engine, prompt: prompt, max_tokens: max_tokens)
+      text = JSON.parse(response.body)["choices"].first["text"]
+      expect(text.split(" ").empty?).to eq(false)
+    end
   end
 end
