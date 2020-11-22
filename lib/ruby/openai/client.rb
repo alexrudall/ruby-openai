32c884f40f1e344babed94709cb6f311ea6d4565
Alex <alexrudall@users.noreply.github.com>
2020-11-22 17:03:37 +0000

    Add all parameters

--- lib/ruby/openai/client.rb
+++ lib/ruby/openai/client.rb
@@ -7,16 +7,14 @@ module OpenAI
       @access_token = access_token || ENV["OPENAI_ACCESS_TOKEN"]
     end
 
-    def call(engine:, prompt:, max_tokens:, version: default_version)
+    def completions(engine:, version: default_version, parameters: {})
       self.class.post(
         "/#{version}/engines/#{engine}/completions",
         headers: {
           "Content-Type" => "application/json",
           "Authorization" => "Bearer #{@access_token}"
         },
-        body: {
-          prompt: prompt, max_tokens: max_tokens
-        }.to_json
+        body: parameters.to_json
       )
     end
 
@@ -29,6 +27,24 @@ module OpenAI
         },
         body: {
           documents: documents, query: query
+        }.to_json
+      )
+    end
+
+    # DEPRECATED.
+    def call(engine:, prompt:, max_tokens:, version: default_version)
+      warn "[DEPRECATION] `Client#call` is deprecated and will be removed in a future version of ruby-openai.
+        Please use `Client#completions` instead, like this:
+        client.completions(engine: 'davinci', parameters: { prompt: 'Once upon a time', max_tokens: 5 })
+      "
+      self.class.post(
+        "/#{version}/engines/#{engine}/completions",
+        headers: {
+          "Content-Type" => "application/json",
+          "Authorization" => "Bearer #{@access_token}"
+        },
+        body: {
+          prompt: prompt, max_tokens: max_tokens
         }.to_json
       )
     end
--- lib/ruby/openai/version.rb
+++ lib/ruby/openai/version.rb
@@ -1,5 +1,5 @@
 module Ruby
   module OpenAI
-    VERSION = "0.2.0".freeze
+    VERSION = "0.3.0".freeze
   end
 end
