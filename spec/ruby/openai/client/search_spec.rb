cc1adc6b101b279415cb8240e1c93f6783467d0f
Alex <7175262+gritops@users.noreply.github.com>
2021-04-07 20:28:32 +0000

    Refactor spec

--- spec/ruby/openai/client/search_spec.rb
+++ spec/ruby/openai/client/search_spec.rb
@@ -1,55 +1,58 @@
 RSpec.describe OpenAI::Client do
   describe "#search" do
-    context "with documents and a query", :vcr do
-      let(:documents) { %w[washington hospital school] }
+    context "with a query" do
       let(:query) { "the president" }
 
-      let(:response) do
-        OpenAI::Client.new.search(
-          engine: engine,
-          documents: documents,
-          query: query
-        )
-      end
-      let(:best_match) { JSON.parse(response.body)["data"].max_by { |d| d["score"] }["document"] }
-      let(:cassette) { "#{engine} search #{query}".downcase }
+      context "with documents", :vcr do
+        let(:documents) { %w[washington hospital school] }
 
-      context "with engine: ada" do
-        let(:engine) { "ada" }
+        let(:response) do
+          OpenAI::Client.new.search(
+            engine: engine,
+            documents: documents,
+            query: query
+          )
+        end
+        let(:best_match) { JSON.parse(response.body)["data"].max_by { |d| d["score"] }["document"] }
+        let(:cassette) { "#{engine} search #{query}".downcase }
 
-        it "finds the best match" do
-          VCR.use_cassette(cassette) do
-            expect(documents[best_match]).to eq("washington")
+        context "with engine: ada" do
+          let(:engine) { "ada" }
+
+          it "finds the best match" do
+            VCR.use_cassette(cassette) do
+              expect(documents[best_match]).to eq("washington")
+            end
           end
         end
-      end
 
-      context "with engine: babbage" do
-        let(:engine) { "babbage" }
+        context "with engine: babbage" do
+          let(:engine) { "babbage" }
 
-        it "finds the best match" do
-          VCR.use_cassette(cassette) do
-            expect(documents[best_match]).to eq("washington")
+          it "finds the best match" do
+            VCR.use_cassette(cassette) do
+              expect(documents[best_match]).to eq("washington")
+            end
           end
         end
-      end
 
-      context "with engine: curie" do
-        let(:engine) { "curie" }
+        context "with engine: curie" do
+          let(:engine) { "curie" }
 
-        it "finds the best match" do
-          VCR.use_cassette(cassette) do
-            expect(documents[best_match]).to eq("washington")
+          it "finds the best match" do
+            VCR.use_cassette(cassette) do
+              expect(documents[best_match]).to eq("washington")
+            end
           end
         end
-      end
 
-      context "with engine: davinci" do
-        let(:engine) { "davinci" }
+        context "with engine: davinci" do
+          let(:engine) { "davinci" }
 
-        it "finds the best match" do
-          VCR.use_cassette(cassette) do
-            expect(documents[best_match]).to eq("washington")
+          it "finds the best match" do
+            VCR.use_cassette(cassette) do
+              expect(documents[best_match]).to eq("washington")
+            end
           end
         end
       end
