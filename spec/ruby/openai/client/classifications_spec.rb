2ec6873cd4603e89abcdf21abc1f7a5854519547
Alex <7175262+gritops@users.noreply.github.com>
2021-04-18 19:43:02 +0000

    Get classifications with file spec working

--- spec/ruby/openai/client/classifications_spec.rb
+++ spec/ruby/openai/client/classifications_spec.rb
@@ -1,39 +1,39 @@
 RSpec.describe OpenAI::Client do
   describe "#classifications", :vcr do
-    let(:query) { "It is a raining day :(" }
+    context "with a file" do
+      let(:query) { "movie is very good" }
+      let(:cassette) { "#{engine} classifications file #{query}".downcase }
+      let(:filename) { "train.jsonl" }
+      let(:file) { File.join(RSPEC_ROOT, "fixtures/files", filename) }
+      let!(:file_id) do
+        response = VCR.use_cassette("files upload classifications") do
+          OpenAI::Client.new.files.upload(parameters: { file: file, purpose: "classifications" })
+        end
+        JSON.parse(response.body)["id"]
+      end
+      let(:response) do
+        OpenAI::Client.new.classifications(
+          parameters: {
+            model: engine,
+            query: query,
+            file: file_id
+          }
+        )
+      end
 
-    # context "with a file" do
-    #   let(:cassette) { "#{engine} classifications file #{query}".downcase }
-    #   let(:filename) { "puppy.jsonl" }
-    #   let(:file) { File.join(RSPEC_ROOT, "fixtures/files", filename) }
-    #   let!(:file_id) do
-    #     response = VCR.use_cassette("files upload classifications") do
-    #       OpenAI::Client.new.files.upload(parameters: { file: file, purpose: "classifications" })
-    #     end
-    #     JSON.parse(response.body)["id"]
-    #   end
-    #   let(:response) do
-    #     OpenAI::Client.new.classifications(
-    #       parameters: {
-    #         model: engine,
-    #         query: query,
-    #         file: file_id
-    #       }
-    #     )
-    #   end
+      context "with engine: curie" do
+        let(:engine) { "curie" }
 
-    #   context "with engine: davinci" do
-    #     let(:engine) { "davinci" }
-
-    #     it "classifications the query" do
-    #       VCR.use_cassette(cassette) do
-    #         expect(response.parsed_response["classifications"][0]).to include("puppy A is happy")
-    #       end
-    #     end
-    #   end
-    # end
+        it "classifies the query" do
+          VCR.use_cassette(cassette) do
+            expect(response["label"]).to eq("Positive")
+          end
+        end
+      end
+    end
 
     context "with examples" do
+      let(:query) { "It is a raining day :(" }
       let(:cassette) { "#{engine} classifications examples #{query}".downcase }
       let(:examples) { [
         ["A happy moment", "Positive"],
@@ -56,7 +56,7 @@ RSpec.describe OpenAI::Client do
 
         it "classifies the query" do
           VCR.use_cassette(cassette) do
-            expect(response.parsed_response["selected_examples"][0]["text"]).to eq("I am feeling awesome")
+            expect(response["label"]).to eq("Negative")
           end
         end
       end
