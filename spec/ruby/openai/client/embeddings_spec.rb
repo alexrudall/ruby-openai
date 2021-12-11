9b1f229f32c8037bfe5337b16dc49b14f7bcef4e
Alex Rudall <alexrudall@gmail.com>
2021-12-11 22:19:06 +0000

    Add Embeddings endpoint

---
+++ spec/ruby/openai/client/embeddings_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe OpenAI::Client do
+  describe "#embeddings", :vcr do
+    let(:input) { "The food was delicious and the waiter..." }
+    let(:cassette) { "#{engine} classifications #{input}".downcase }
+    let(:response) do
+      OpenAI::Client.new.embeddings(
+        engine: engine,
+        parameters: {
+          engine: engine,
+          input: input
+        }
+      )
+    end
+
+    context "with engine: babbage-similarity" do
+      let(:engine) { "babbage-similarity" }
+
+      it "succeeds" do
+        VCR.use_cassette(cassette) do
+          expect(response["data"][0]["object"]).to eq("embedding")
+        end
+      end
+    end
+  end
+end
