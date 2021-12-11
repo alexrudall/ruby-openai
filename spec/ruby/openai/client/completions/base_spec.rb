c8b7a5b9f1005278a6db86766c42595b0245f4f0
Alex Rudall <alexrudall@gmail.com>
2021-12-11 14:26:29 +0000

    Move base and instruct specs to separate files

--- spec/ruby/openai/client/completions/base_spec.rb
+++ spec/ruby/openai/client/completions/base_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe OpenAI::Client do
-  describe "#completions" do
+  describe "#completions: base engines" do
     context "with a prompt and max_tokens", :vcr do
       let(:prompt) { "Once upon a time" }
       let(:max_tokens) { 5 }
@@ -48,46 +48,6 @@ RSpec.describe OpenAI::Client do
 
       context "with engine: davinci" do
         let(:engine) { "davinci" }
-
-        it "succeeds" do
-          VCR.use_cassette(cassette) do
-            expect(text.split.empty?).to eq(false)
-          end
-        end
-      end
-
-      context "with engine: ada-instruct-beta" do
-        let(:engine) { "ada-instruct-beta" }
-
-        it "succeeds" do
-          VCR.use_cassette(cassette) do
-            expect(text.split.empty?).to eq(false)
-          end
-        end
-      end
-
-      context "with engine: babbage-instruct-beta" do
-        let(:engine) { "babbage-instruct-beta" }
-
-        it "succeeds" do
-          VCR.use_cassette(cassette) do
-            expect(text.split.empty?).to eq(false)
-          end
-        end
-      end
-
-      context "with engine: curie-instruct-beta-v2" do
-        let(:engine) { "curie-instruct-beta-v2" }
-
-        it "succeeds" do
-          VCR.use_cassette(cassette) do
-            expect(text.split.empty?).to eq(false)
-          end
-        end
-      end
-
-      context "with engine: davinci-instruct-beta-v3" do
-        let(:engine) { "davinci-instruct-beta-v3" }
 
         it "succeeds" do
           VCR.use_cassette(cassette) do
