96283875da589ddbbcc3176a384f5f9993947d37
Alex Rudall <alexrudall@gmail.com>
2022-11-13 16:59:30 +0000

    Add to README

--- spec/ruby/openai/client/images_spec.rb
+++ spec/ruby/openai/client/images_spec.rb
@@ -5,16 +5,18 @@ RSpec.describe OpenAI::Client do
         OpenAI::Client.new.images.generate(
           parameters: {
             prompt: prompt,
+            size: size,
           }
         )
       }
-      let(:cassette) { "images generations #{prompt}" }
+      let(:cassette) { "images generate #{prompt}" }
       let(:prompt) { "A baby sea otter cooking pasta wearing a hat of some sort" }
+      let(:size) { "256x256" }
 
       it "succeeds" do
         VCR.use_cassette(cassette) do
           r = JSON.parse(response.body)
-          expect(r["data"][0]["url"]).to include("dalle")
+          expect(r.dig("data", 0, "url")).to include("dalle")
         end
       end
     end
