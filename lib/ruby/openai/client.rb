0e0f0c4ee6938d3910f461dbabe9c68dd9f11dfd
Alex Rudall <alexrudall@gmail.com>
2022-12-26 17:42:08 +0000

    Remove #search

--- lib/ruby/openai/client.rb
+++ lib/ruby/openai/client.rb
@@ -39,14 +39,6 @@ module OpenAI
       OpenAI::Client.post(path: "/moderations", parameters: parameters)
     end
 
-    def search(engine:, parameters: {})
-      warn "[DEPRECATION WARNING] [ruby-openai] `Client#search` is deprecated and will
-      be removed from the OpenAI API on 3 December 2022 and from ruby-openai v3.0.
-      More information: https://help.openai.com/en/articles/6272952-search-transition-guide"
-
-      OpenAI::Client.post(path: "/engines/#{engine}/search", parameters: parameters)
-    end
-
     def self.get(path:)
       HTTParty.get(
         uri(path: path),
@@ -79,24 +71,6 @@ module OpenAI
         "Authorization" => "Bearer #{Ruby::OpenAI.configuration.access_token}",
         "OpenAI-Organization" => Ruby::OpenAI.configuration.organization_id
       }
-    end
-
-    private
-
-    def deprecate_engine(engine:, method:, parameters:)
-      return parameters unless engine
-
-      parameters = { model: engine }.merge(parameters)
-
-      warn "[DEPRECATION WARNING] [ruby-openai] Passing `engine` directly to `Client##{method}` is
-      deprecated and will be removed in ruby-openai 3.0. Pass `model` within `parameters` instead:
-      client.completions(parameters: { #{parameters.map { |k, v| "#{k}: \"#{v}\"" }.join(', ')} })"
-
-      parameters
-    end
-
-    def documents_or_file(documents: nil, file: nil)
-      documents ? { documents: documents } : { file: file }
     end
   end
 end
