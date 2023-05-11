8b4096d4ff63d2d7067be4b28c2b3b4319f97835
Takatsugu Nokubi <takatsugu.nokubi@appbrew.io>
2023-05-11 14:44:21 +0900

    call azure api

--- spec/openai/client/chat_spec.rb
+++ spec/openai/client/chat_spec.rb
@@ -3,8 +3,10 @@ RSpec.describe OpenAI::Client do
     context "with messages", :vcr do
       let(:messages) { [{ role: "user", content: "Hello!" }] }
       let(:stream) { false }
+      let(:deployment_id) { nil }
       let(:response) do
         OpenAI::Client.new.chat(
+          deployment_id: deployment_id,
           parameters: {
             model: model,
             messages: messages,
@@ -43,6 +45,35 @@ RSpec.describe OpenAI::Client do
 
       context "with model: gpt-3.5-turbo-0301" do
         let(:model) { "gpt-3.5-turbo-0301" }
+
+        it "succeeds" do
+          VCR.use_cassette(cassette) do
+            expect(content.split.empty?).to eq(false)
+          end
+        end
+      end
+
+      context "with Azure" do
+        let(:model) { "gpt-3.5-turbo-0301" }
+        let(:deployment_id) { ENV.fetch("AZURE_DEPLOYMENT_ID") }
+
+        before do
+          OpenAI.configure do |config|
+            config.api_type = :azure
+            config.api_version = "2023-03-15-preview"
+            config.uri_base = ENV.fetch("AZURE_URI_BASE")
+            config.access_token = ENV.fetch("AZURE_ACCESS_TOKEN")
+          end
+        end
+
+        after do
+          OpenAI.configure do |config|
+            config.api_type = :azure
+            config.api_version = "2023-03-15-preview"
+            config.uri_base = nil
+            config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN", nil)
+          end
+        end
 
         it "succeeds" do
           VCR.use_cassette(cassette) do
