b62877470783c12a7d3a24aff198dee35007d361
Alex <alexrudall@users.noreply.github.com>
2023-05-16 13:46:43 +0800

    Add request_timeout

--- spec/openai/client/client_spec.rb
+++ spec/openai/client/client_spec.rb
@@ -3,23 +3,33 @@ RSpec.describe OpenAI::Client do
     before do
       OpenAI.configure do |config|
         config.access_token = "default_access_token"
-        config.uri_base = "https://api.openai.com"
       end
     end
 
     it "does not confuse the clients" do
       default = OpenAI::Client.new
-      client1 = OpenAI::Client.new(access_token: "access_token1", uri_base: "https://oai.hconeai.com/")
-      client2 = OpenAI::Client.new(access_token: "access_token2", uri_base: "https://example.com")
+      client1 = OpenAI::Client.new(
+        access_token: "access_token1",
+        request_timeout: 60,
+        uri_base: "https://oai.hconeai.com/"
+      )
+      client2 = OpenAI::Client.new(
+        access_token: "access_token2",
+        request_timeout: 1,
+        uri_base: "https://example.com/"
+      )
 
       expect(default.access_token).to eq("default_access_token")
-      expect(default.uri_base).to eq("https://api.openai.com")
+      expect(default.request_timeout).to eq(OpenAI::Configuration::DEFAULT_REQUEST_TIMEOUT)
+      expect(default.uri_base).to eq(OpenAI::Configuration::DEFAULT_URI_BASE)
 
       expect(client1.access_token).to eq("access_token1")
+      expect(client1.request_timeout).to eq(60)
       expect(client1.uri_base).to eq("https://oai.hconeai.com/")
 
       expect(client2.access_token).to eq("access_token2")
-      expect(client2.uri_base).to eq("https://example.com")
+      expect(client2.request_timeout).to eq(1)
+      expect(client2.uri_base).to eq("https://example.com/")
     end
   end
 end
