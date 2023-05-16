5f1c585ecdea9751753c4244f02b1a8c0456d619
Alex <alexrudall@users.noreply.github.com>
2023-05-16 13:47:59 +0800

    Refactor

--- spec/openai/client/client_spec.rb
+++ spec/openai/client/client_spec.rb
@@ -6,19 +6,23 @@ RSpec.describe OpenAI::Client do
       end
     end
 
-    it "does not confuse the clients" do
-      default = OpenAI::Client.new
-      client1 = OpenAI::Client.new(
+    let!(:default) { OpenAI::Client.new }
+    let!(:client1) do
+      OpenAI::Client.new(
         access_token: "access_token1",
         request_timeout: 60,
         uri_base: "https://oai.hconeai.com/"
       )
-      client2 = OpenAI::Client.new(
+    end
+    let!(:client2) do
+      OpenAI::Client.new(
         access_token: "access_token2",
         request_timeout: 1,
         uri_base: "https://example.com/"
       )
+    end
 
+    it "does not confuse the clients" do
       expect(default.access_token).to eq("default_access_token")
       expect(default.request_timeout).to eq(OpenAI::Configuration::DEFAULT_REQUEST_TIMEOUT)
       expect(default.uri_base).to eq(OpenAI::Configuration::DEFAULT_URI_BASE)
