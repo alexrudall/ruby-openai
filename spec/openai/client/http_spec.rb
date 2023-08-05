1cd9216b301a2f8247c703960193e6fbd9f4e727
Alex <alexrudall@users.noreply.github.com>
2023-08-05 18:59:35 +0100

    Fix spec

--- spec/openai/client/http_spec.rb
+++ spec/openai/client/http_spec.rb
@@ -189,7 +189,7 @@ RSpec.describe OpenAI::HTTP do
 
   describe ".uri" do
     let(:path) { "/chat" }
-    let(:uri) { OpenAI::Client.send(:uri, path: path) }
+    let(:uri) { OpenAI::Client.new.send(:uri, path: path) }
 
     it { expect(uri).to eq("https://api.openai.com/v1/chat") }
 
@@ -217,7 +217,7 @@ RSpec.describe OpenAI::HTTP do
       end
 
       let(:path) { "/chat" }
-      let(:uri) { OpenAI::Client.send(:uri, path: path) }
+      let(:uri) { OpenAI::Client.new.send(:uri, path: path) }
 
       context "with a trailing slash" do
         let(:uri_base) { "https://custom-domain.openai.azure.com/openai/deployments/gpt-35-turbo/" }
@@ -236,7 +236,7 @@ RSpec.describe OpenAI::HTTP do
       OpenAI.configuration.api_type = :nil
     end
 
-    let(:headers) { OpenAI::Client.send(:headers) }
+    let(:headers) { OpenAI::Client.new.send(:headers) }
 
     it {
       expect(headers).to eq({ "Authorization" => "Bearer #{OpenAI.configuration.access_token}",
@@ -252,7 +252,7 @@ RSpec.describe OpenAI::HTTP do
         OpenAI.configuration.api_type = nil
       end
 
-      let(:headers) { OpenAI::Client.send(:headers) }
+      let(:headers) { OpenAI::Client.new.send(:headers) }
 
       it {
         expect(headers).to eq({ "Content-Type" => "application/json",
