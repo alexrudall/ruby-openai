a4d467db31ba8fc10121554bce0ef7286c7d6b10
Ates Goral <ates.goral@shopify.com>
2023-10-19 20:17:04 -0400

    Remove completion test in finetunes suite

--- spec/openai/client/finetunes_spec.rb
+++ spec/openai/client/finetunes_spec.rb
@@ -95,25 +95,5 @@ RSpec.describe OpenAI::Client do
         end
       end
     end
-
-    describe "#completions" do
-      let(:prompt) { "I love Mondays" }
-      let(:cassette) { "finetune completions #{prompt}".downcase }
-      let(:model) { "ada:ft-user-jxm65ijkzc1qrfhc0ij8moic-2021-12-11-20-11-52" }
-      let(:response) do
-        OpenAI::Client.new.completions(
-          parameters: {
-            model: model,
-            prompt: prompt
-          }
-        )
-      end
-
-      it "succeeds" do
-        VCR.use_cassette(cassette) do
-          expect(response["model"]).to eq(model)
-        end
-      end
-    end
   end
 end
