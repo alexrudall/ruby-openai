4766cf5590d05627eb9bd56e9f5768d90962e85d
Alex Rudall <alexrudall@gmail.com>
2023-11-09 20:56:24 +0000

    Get assistants_spec passing vs the API

--- spec/openai/client/assistants_spec.rb
+++ spec/openai/client/assistants_spec.rb
@@ -1,8 +1,17 @@
 RSpec.describe OpenAI::Client do
   describe "#assistants" do
+    let(:assistant_id) do
+      VCR.use_cassette("#{cassette} setup") do
+        OpenAI::Client.new.assistants.create(parameters: { model: "gpt-4",
+          name: "OpenAI-Ruby test assistant" })["id"]
+      end
+    end
+
     describe "#list", :vcr do
       let(:response) { OpenAI::Client.new.assistants.list }
       let(:cassette) { "assistants list" }
+
+      before { assistant_id }
 
       it "succeeds" do
         VCR.use_cassette(cassette) do
@@ -13,7 +22,7 @@ RSpec.describe OpenAI::Client do
 
     describe "#retrieve" do
       let(:cassette) { "assistants retrieve" }
-      let(:response) { OpenAI::Client.new.assistants.retrieve(id: "asst_KfrvKUIYCRCzmwuGe9uuHBHJ") }
+      let(:response) { OpenAI::Client.new.assistants.retrieve(id: assistant_id) }
 
       it "succeeds" do
         VCR.use_cassette(cassette) do
@@ -32,7 +41,6 @@ RSpec.describe OpenAI::Client do
       it "succeeds" do
         VCR.use_cassette(cassette) do
           expect(response["object"]).to eq "assistant"
-          expect(response["id"]).to eq "asst_SGTQseRVgIIasVsVHPDtQNis"
         end
       end
     end
@@ -41,7 +49,7 @@ RSpec.describe OpenAI::Client do
       let(:cassette) { "assistants modify" }
       let(:response) do
         OpenAI::Client.new.assistants.modify(
-          id: "asst_SGTQseRVgIIasVsVHPDtQNis",
+          id: assistant_id,
           parameters: { model: "gpt-3.5-turbo", name: "Test Assistant for OpenAI-Ruby" }
         )
       end
@@ -49,7 +57,6 @@ RSpec.describe OpenAI::Client do
       it "succeeds" do
         VCR.use_cassette(cassette) do
           expect(response["object"]).to eq "assistant"
-          expect(response["id"]).to eq "asst_SGTQseRVgIIasVsVHPDtQNis"
         end
       end
     end
@@ -57,13 +64,12 @@ RSpec.describe OpenAI::Client do
     describe "#delete" do
       let(:cassette) { "assistants delete" }
       let(:response) do
-        OpenAI::Client.new.assistants.delete(id: "asst_euYzVdG6KgykipVXq8zhKHwy")
+        OpenAI::Client.new.assistants.delete(id: assistant_id)
       end
 
       it "succeeds" do
         VCR.use_cassette(cassette) do
           expect(response["object"]).to eq "assistant.deleted"
-          expect(response["id"]).to eq "asst_euYzVdG6KgykipVXq8zhKHwy"
         end
       end
     end
