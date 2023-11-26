4fe84da2ea034d3aca77110e3b06dc421d5faa4e
Alex Rudall <alexrudall@gmail.com>
2023-11-26 18:17:04 +0000

    Rubocop

--- lib/openai.rb
+++ lib/openai.rb
@@ -26,7 +26,7 @@ module OpenAI
       raise e unless e.response.is_a?(Hash)
 
       logger = Logger.new($stdout)
-      logger.formatter = proc do |severity, datetime, progname, msg|
+      logger.formatter = proc do |_severity, _datetime, _progname, msg|
         "\033[31mOpenAI HTTP Error (spotted in ruby-openai #{VERSION}): #{msg}\n\033[0m"
       end
       logger.error(e.response[:body])
