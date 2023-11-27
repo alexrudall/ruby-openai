9a8154e7947eeca9eaf154c32d59deb001cd9f42
Stefan <stefan@kaprobleem.com>
2023-11-27 16:08:51 +0100

    Files for assistants can now be PDFs,... as we only validate .jsonl files

--- lib/openai/files.rb
+++ lib/openai/files.rb
@@ -9,7 +9,7 @@ module OpenAI
     end
 
     def upload(parameters: {})
-      validate(file: parameters[:file])
+      validate(file: parameters[:file]) if parameters[:file].include?(".jsonl")
 
       @client.multipart_post(
         path: "/files",
