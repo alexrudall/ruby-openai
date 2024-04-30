9a7908fe6d68dff2bfa7c84d64918a1c457dfab4
WillyWG <willywg@gmail.com>
2024-04-30 11:44:34 -0500

    feat(vector_stores_spec.rb): add tests for vector stores endpoints in OpenAI client
    This commit adds tests for the vector stores endpoints in the OpenAI client. The tests cover the following functionality:
    
    - `#list`: verifies that the response contains a vector store object
    - `#retrieve`: verifies that the response contains a vector store object
    - `#create`: verifies that the response contains a vector store object
    - `#modify`: verifies that the response contains a vector store object
    - `#delete`: verifies that the response contains a vector store.deleted object
    
    These tests ensure that the vector stores functionality in the OpenAI client is working correctly.

---
+++ spec/openai/client/vector_stores_spec.rb
@@ -0,0 +1,75 @@
+RSpec.describe OpenAI::Client do
+  describe "#vector_stores" do
+    let(:vector_store_id) do
+      VCR.use_cassette("#{cassette} setup") do
+        OpenAI::Client.new.vector_stores.create(parameters: {})["id"]
+      end
+    end
+
+    describe "#list" do
+      let(:cassette) { "vector_stores list" }
+      let(:response) do
+        OpenAI::Client.new.vector_stores.list(parameters: {})
+      end
+
+      it "succeeds" do
+        VCR.use_cassette(cassette) do
+          expect(response.dig("data", 0, "object")).to eq("vector_store")
+        end
+      end
+    end
+
+    describe "#retrieve" do
+      let(:cassette) { "vector_stores retrieve" }
+      let(:response) { OpenAI::Client.new.vector_stores.retrieve(id: vector_store_id) }
+
+      it "succeeds" do
+        VCR.use_cassette(cassette) do
+          expect(response["object"]).to eq("vector_store")
+        end
+      end
+    end
+
+    describe "#create" do
+      let(:cassette) { "vector_stores create" }
+      let(:response) do
+        OpenAI::Client.new.vector_stores.create(parameters: {})
+      end
+
+      it "succeeds" do
+        VCR.use_cassette(cassette) do
+          expect(response["object"]).to eq "vector_store"
+        end
+      end
+    end
+
+    describe "#modify" do
+      let(:cassette) { "vector_stores modify" }
+      let(:response) do
+        OpenAI::Client.new.vector_stores.modify(
+          id: vector_store_id,
+          parameters: { metadata: { modified: "true" } }
+        )
+      end
+
+      it "succeeds" do
+        VCR.use_cassette(cassette) do
+          expect(response["object"]).to eq "vector_store"
+        end
+      end
+    end
+
+    describe "#delete" do
+      let(:cassette) { "vector_stores delete" }
+      let(:response) do
+        OpenAI::Client.new.vector_stores.delete(id: vector_store_id)
+      end
+
+      it "succeeds" do
+        VCR.use_cassette(cassette) do
+          expect(response["object"]).to eq "vector_store.deleted"
+        end
+      end
+    end
+  end
+end
