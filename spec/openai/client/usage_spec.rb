4c1f73e8311cbd40da5775367e8644c9ddc83268
Alex Rudall <alexrudall@gmail.com>
2025-02-10 10:16:29 +0000

    Fix specs without real token

--- spec/openai/client/usage_spec.rb
+++ spec/openai/client/usage_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe OpenAI::Client do
   describe "#usage" do
-    let(:one_day_ago) { Time.now.to_i - 86_400 }
+    let(:some_time_ago) { 1_739_096_063 }
     let(:client) { OpenAI::Client.new }
 
     shared_examples "usage endpoint" do |endpoint_name|
       describe "##{endpoint_name}" do
         let(:response) do
-          client.usage.public_send(endpoint_name, parameters: { start_time: one_day_ago })
+          client.usage.public_send(endpoint_name, parameters: { start_time: some_time_ago })
         end
 
         it "retrieves #{endpoint_name} usage data" do
