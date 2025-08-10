97b06d68d293e4fa2a090e8a177ab5ca82bb545a
Alex Rudall <alexrudall@gmail.com>
2025-08-10 12:01:32 +0100

    Typo

--- lib/openai/realtime.rb
+++ lib/openai/realtime.rb
@@ -9,7 +9,6 @@ module OpenAI
     # This method sets up a new session for real-time voice interaction with an OpenAI model.
     # It returns session details that can be used to establish a WebRTC connection.
     #
-    #
     # @param parameters [Hash] parameters for the session (see: https://platform.openai.com/docs/api-reference/realtime-sessions/create)
     # @return [Hash] Session details including session ID, ICE servers, and other
     # connection information
